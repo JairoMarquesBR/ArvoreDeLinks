534100943fa2967f6e122990e1027e9c0adf173f
Jairo Marques <jairomarques.servico@gmail.com>
2022-12-24 17:46:11 -0300

    Fixando a barra de navegação

--- pages/nav.php
+++ pages/nav.php
@@ -1,11 +1,13 @@
 <!--Navbar-->
-<nav class="deep-purple darken-4">
-    <div class="nav-wrapper container">
-        <a href="#" class="brand-logo right">Jairo Marques</a>
-        <ul id="nav-mobile" class="left">
-            <li><a href="#" class="modal-trigger" data-target="modal1"><i class="large material-icons">help</i></a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<div class="navbar-fixed">
+    <nav class="deep-purple darken-4">
+        <div class="nav-wrapper container">
+            <a href="#" class="brand-logo right">Jairo Marques</a>
+            <ul id="nav-mobile" class="left">
+                <li><a href="#" class="modal-trigger" data-target="modal1"><i class="large material-icons">help</i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
 <!--Navbar Fim-->
