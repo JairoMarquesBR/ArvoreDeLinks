4ffe5917e222d7e7d2d6f1be72e8c9af820468d6
Jairo Marques <jairomarques.servico@gmail.com>
2022-12-24 17:50:40 -0300

    Editando a modal

--- pages/dependencesFramework.php
+++ pages/dependencesFramework.php
@@ -1,11 +1,10 @@
 <!--Modal 01-->
 <div id="modal1" class="modal modal-fixed-footer">
         <div class="modal-content">
-            <h4>Observações</h4>
-            <p>A página foi criada com o intuito de facilitar o acesso as minhas redes, todo o código foi escrito por mim com o auxilio do framework materializecss em dezembro de 2022. <br>
-        Qualquer erro ou bug, por favor reportar no meu whatsapp!</p>
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
         </div>
         <div class="modal-footer">
-            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Entendi</a>
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
         </div>
     </div>
